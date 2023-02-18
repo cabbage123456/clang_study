35619cfbab6fdb84c57adb7f03a333761bb5b9be
cabbage123456 <125133653+cabbage123456@users.noreply.github.com>
2023-02-18 19:37:25 +0800

    Add files via upload

--- 1-函数/main.c
+++ 1-函数/main.c
@@ -1,3 +1,16 @@
-#include <stio.h>
-
-
+#include <stdio.h>
+int main(void){
+    int a,b,c;
+    printf("输入a,b");
+    scanf("%d%d",&a,&b);
+    c=(a/10*10)+(a%10*1000)+(b/10+b%10*100);
+    printf("%d\n",c);
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
